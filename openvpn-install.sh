2d39c80ca3e988591ea230d944aeec2faa4bea3a
MyGatherBk <49818403+MyGatherBk@users.noreply.github.com>
2019-05-27 01:29:44 +0700

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -392,69 +392,14 @@ cipher AES-256-CBC
 setenv opt block-outside-dns
 key-direction 1
 verb 3" > /etc/openvpn/client-common.txt
-case $OPENVPNSYSTEM in
-		2)
-		echo "auth-user-pass" >> /etc/openvpn/client-common.txt
-		;;
-	esac
-
-	cd
-	apt-get -y install nginx
-	cat > /etc/nginx/nginx.conf <<END
-user www-data;
-worker_processes 2;
-pid /var/run/nginx.pid;
-events {
-	multi_accept on;
-        worker_connections 1024;
-}
-http {
-	autoindex on;
-        sendfile on;
-        tcp_nopush on;
-        tcp_nodelay on;
-        keepalive_timeout 65;
-        types_hash_max_size 2048;
-        server_tokens off;
-        include /etc/nginx/mime.types;
-        default_type application/octet-stream;
-        access_log /var/log/nginx/access.log;
-        error_log /var/log/nginx/error.log;
-        client_max_body_size 32M;
-	client_header_buffer_size 8m;
-	large_client_header_buffers 8 8m;
-	fastcgi_buffer_size 8m;
-	fastcgi_buffers 8 8m;
-	fastcgi_read_timeout 600;
-        include /etc/nginx/conf.d/*.conf;
-}
-END
-	mkdir -p /home/vps/public_html
-	echo "<pre>by Ekkachai Chompoowiset | Donate via TrueMoney Wallet : 097-206-5255</pre>" > /home/vps/public_html/index.html
-	echo "<?phpinfo(); ?>" > /home/vps/public_html/info.php
-	args='$args'
-	uri='$uri'
-	document_root='$document_root'
-	fastcgi_script_name='$fastcgi_script_name'
-	cat > /etc/nginx/conf.d/vps.conf <<END
-server {
-    listen       85;
-    server_name  127.0.0.1 localhost;
-    access_log /var/log/nginx/vps-access.log;
-    error_log /var/log/nginx/vps-error.log error;
-    root   /home/vps/public_html;
-    location / {
-        index  index.html index.htm index.php;
-	try_files $uri $uri/ /index.php?$args;
-    }
-    location ~ \.php$ {
-        include /etc/nginx/fastcgi_params;
-        fastcgi_pass  127.0.0.1:9000;
-        fastcgi_index index.php;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-    }
-}
-END
+	# Generates the custom client.ovpn
+	newclient "$CLIENT"
+	echo
+	echo "Finished!"
+	echo
+	echo "Your client configuration is available at:" ~/"$CLIENT.ovpn"
+	echo "If you want to add more clients, you simply need to run this script again!"
+fi
 # install squid3
 apt-get -y install squid3
 cat > /etc/squid3/squid.conf <<-END
