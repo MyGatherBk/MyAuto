1ba19f00ac49fb199199fdb6cf9ade0303d5eb51
MyGatherBk <49818403+MyGatherBk@users.noreply.github.com>
2019-05-28 00:01:34 +0700

    Update usernew.sh

--- usernew.sh
+++ usernew.sh
@@ -9,13 +9,13 @@ IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
-echo -e ""
+echo ""
+echo "~¤~ ๏[-ิ_•ิ]๏ ~¤~ Admin MyGatherBK ~¤~ ๏[-ิ_•ิ]๏ ~¤~"
+echo ""
 echo -e "====สร้าง User===="
 echo -e "Host: $IP" 
-#echo -e "Port OpenSSH: 22,80"
-#echo -e "Port Dropbear: 143,443"
-echo -e "Squid: 8080,3128"
-#echo -e "Config OpenVPN (TCP 1194): http://$IP:81/client.ovpn"
+echo -e "Port OpenSSH: 22"
+echo -e "Squid: 8080"
 echo -e "Username: $Login "
 echo -e "Password: $Pass"
 echo -e "-----------------------------"
