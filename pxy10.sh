5ee3331719d3e70c6a726faa739fe39053af15ec
MyGatherBk <49818403+MyGatherBk@users.noreply.github.com>
2020-07-24 23:14:53 +0700

    Update pxy10.sh

--- pxy10.sh
+++ pxy10.sh
@@ -10,6 +10,6 @@ MYIP2="s/xxxxxxxxx/$MYIP/g";
 # Install Squid3
 cd
 apt-get -y install squid3
-wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/MyGatherBk/MyAuto/master/squid3.conf"
-sed -i $MYIP2 /etc/squid3/squid.conf;
-systemctl squid3 restart
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/MyGatherBk/MyAuto/master/squid.conf"
+sed -i $MYIP2 /etc/squid/squid.conf;
+systemctl squid restart
