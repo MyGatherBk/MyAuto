6cfb5f0a67d07a10f09648f4b5205a2d008c15f5
MyGatherBk <49818403+MyGatherBk@users.noreply.github.com>
2020-07-24 23:21:15 +0700

    Update pxy10.sh

--- pxy10.sh
+++ pxy10.sh
@@ -4,12 +4,13 @@
 # ==================================================
 # Initializing Var
 
-MYIP=$(wget -qO- ipv4.icanhazip.com);
-MYIP2="s/xxxxxxxxx/$MYIP/g";
-
+# IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+# if [[ "$IP" = "" ]]; then
+IP=$(wget -4qO- "http://whatismyip.akamai.com/")
+# fi
 # Install Squid3
 cd
 apt-get -y install squid
 wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/MyGatherBk/MyAuto/master/squid.conf"
-sed -i $MYIP2 /etc/squid/squid.conf;
+sed -i $IP /etc/squid/squid.conf;
 systemctl squid restart
