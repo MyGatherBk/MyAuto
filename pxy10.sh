4e850fadf941a1da61f36ef1c4e4ae6f2de399d3
MyGatherBk <49818403+MyGatherBk@users.noreply.github.com>
2020-07-24 23:35:50 +0700

    Update pxy10.sh

--- pxy10.sh
+++ pxy10.sh
@@ -9,8 +9,6 @@ IP=$(wget -4qO- "http://whatismyip.akamai.com/")
 # fi
 
 echo -e "${GREEN} ไอพีเซิฟ: $IP "
-	read -p "Port Proxy : " -e -i 8080 PROXY
-# Install Squid3
 	if [[ "$VERSION_ID" = 'VERSION_ID="10"' || "$VERSION_ID" = 'VERSION_ID="8"' || "$VERSION_ID" = 'VERSION_ID="14.04"' ]]; then
 		if [[ -e /etc/squid3/squid.conf ]]; then
 			apt-get -y remove --purge squid3
@@ -59,3 +57,49 @@ END
 			/etc/init.d/openvpn restart
 			/etc/init.d/nginx restart
 		fi
+
+	elif [[ "$VERSION_ID" = 'VERSION_ID="9"' || "$VERSION_ID" = 'VERSION_ID="16.04"' || "$VERSION_ID" = 'VERSION_ID="18.04"' ]]; then
+		if [[ -e /etc/squid/squid.conf ]]; then
+			apt-get -y remove --purge squid
+		fi
+echo ""
+echo -e "\033[0;32m { Install PROXY }${NC} "
+echo ""
+		apt-get -y install squid
+		cat > /etc/squid/squid.conf <<END
+http_port 8080
+acl localhost src 127.0.0.1/32 ::1
+acl to_localhost dst 127.0.0.0/8 0.0.0.0/32 ::1
+acl localnet src 10.0.0.0/8
+acl localnet src 172.16.0.0/12
+acl localnet src 192.168.0.0/16
+acl SSL_ports port 443
+acl Safe_ports port 80
+acl Safe_ports port 21
+acl Safe_ports port 443
+acl Safe_ports port 70
+acl Safe_ports port 210
+acl Safe_ports port 1025-65535
+acl Safe_ports port 280
+acl Safe_ports port 488
+acl Safe_ports port 591
+acl Safe_ports port 777
+acl CONNECT method CONNECT
+acl SSH dst xxxxxxxxx-xxxxxxxxx/255.255.255.255
+http_access allow SSH
+http_access allow localnet
+http_access allow localhost
+http_access deny all
+refresh_pattern ^ftp:           1440    20%     10080
+refresh_pattern ^gopher:        1440    0%      1440
+refresh_pattern -i (/cgi-bin/|\?) 0     0%      0
+refresh_pattern .               0       20%     4320
+END
+		IP2="s/xxxxxxxxx/$IP/g";
+		sed -i $IP2 /etc/squid/squid.conf;
+		/etc/init.d/squid restart
+		/etc/init.d/openvpn restart
+		/etc/init.d/nginx restart
+	fi
+
+fi
