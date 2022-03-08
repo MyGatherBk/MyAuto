dae976eb7eb6e2302b8a33ab08ac94fd64b0b384
MyGatherBk <49818403+MyGatherBk@users.noreply.github.com>
2022-03-09 01:41:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -418,7 +418,6 @@ WantedBy=multi-user.target" >> /etc/systemd/system/openvpn-iptables.service
 dev tun
 proto $protocol
 remote $ip $port
-http-proxy $IP $PROXY
 http-proxy-option CUSTOM-HEADER CONNECT HTTP/1.0
 http-proxy-option CUSTOM-HEADER Host opensignal.com
 http-proxy-option CUSTOM-HEADER X-Online-Host opensignal.com
@@ -439,39 +438,9 @@ echo ""
 echo "-------------- { Install PROXY } -------------- "
 echo ""
 # install squid3
-apt-get -y install squid3
-cat > /etc/squid3/squid.conf <<-END
-acl localhost src 127.0.0.1/32 ::1
-acl to_localhost dst 127.0.0.0/8 0.0.0.0/32 ::1
-acl SSL_ports port 443
-acl Safe_ports port 80
-acl Safe_ports port 21
-acl Safe_ports port 443
-acl Safe_ports port 70
-acl Safe_ports port 210
-acl Safe_ports port 1025-65535
-acl Safe_ports port 280
-acl Safe_ports port 488
-acl Safe_ports port 591
-acl Safe_ports port 777
-acl CONNECT method CONNECT
-acl SSH dst xxxxxxxxx-xxxxxxxxx/32
-http_access allow SSH
-http_access allow manager localhost
-http_access deny manager
-http_access allow localhost
-http_access deny all
-http_port 8080
-http_port 8000
-http_port 80
-http_port 3128
-coredump_dir /var/spool/squid3
-refresh_pattern ^ftp: 1440 20% 10080
-refresh_pattern ^gopher: 1440 0% 1440
-refresh_pattern -i (/cgi-bin/|\?) 0 0% 0
-refresh_pattern . 0 20% 4320
-visible_hostname daybreakersx
-END
+apt-get -y install squid3;
+cp /etc/squid3/squid.conf
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/MyGatherBk/MyAuto/master/squid.conf"
 sed -i $IP2 /etc/squid3/squid.conf;
 service squid3 restart
 echo ""
