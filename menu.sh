62b1f09b190798312aa75889191bafe35cb229d4
MyGatherBk <49818403+MyGatherBk@users.noreply.github.com>
2022-03-10 00:15:52 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -2,10 +2,7 @@
 #Menu
 
 echo -e "================================MENU====================================="
-echo -e "* menu      : เรียกดู menu"
-echo -e "* addc      : เพิ่มบัญชีผู้ใช้ OpenVPN"
-echo -e "* dec       : ลบบัญชีผู้ใช้ client"
-echo -e "* deo       : ลบ SCRIPT OPENVPN"
-echo -e "* speedtest : ทดสอบความเร็วเน็ต"
+echo -e "» menu          : Displays a list of commands"
+echo -e "» dec           : modify"
 echo -e "========================================================================="
 echo -e ""
