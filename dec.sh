32fd87e50fae63964779f8534c3cfa772e48f164
MyGatherBk <49818403+MyGatherBk@users.noreply.github.com>
2022-03-10 00:43:45 +0700

    Update dec.sh

--- dec.sh
+++ dec.sh
@@ -8,15 +8,7 @@ echo -e "${RED} =============== OS-32 & 64-bit =================    "
 echo -e "${GREEN} ไอพีเซิฟ: $IP "
 echo -e "${NC} "
 echo ""
-echo -e "${GRAY}ปรับเปลี่ยนระบบของเซิฟเวอร์ ${NC} "
-echo ""
-echo -e "|${GRAY}1${NC}| 1 ไฟล์เชื่อมต่อได้ไม่จำกัดเครื่อง"
-echo ""
-read -p "เลือกหัวข้อที่ต้องการใช้งาน : " CHANGESYSTEMSERVER
 
-case $CHANGESYSTEMSERVER in
-
-	1)
 
 sed -i '28d' /etc/openvpn/server/server.conf
 sed -i '28d' /etc/openvpn/server/server.conf
@@ -24,7 +16,7 @@ sed -i '28d' /etc/openvpn/server/server.conf
 sed -i '20d' /etc/openvpn/server/client-common.txt
 echo "duplicate-cn" >> /etc/openvpn/server.conf
 echo ""
-echo "ปรับเปลี่ยนระบบของเซิฟเวอร์เป็นรูปแบบที่ 3 เรียบร้อย"
+echo "ปรับเปลี่ยนระบบของเซิฟเวอร์เป็นรูปแบบที่ 1 เรียบร้อย"
 echo ""
 service openvpn restart
-
+exit
