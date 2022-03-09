897f16fdea70e6d43df67e5f13ed78261eb03b46
MyGatherBk <49818403+MyGatherBk@users.noreply.github.com>
2022-03-10 00:25:40 +0700

    Update dec.sh

--- dec.sh
+++ dec.sh
@@ -1,42 +1,74 @@
-#!/bin/bash
-#Script ลบบัญชีผู้ใช้ client
-# Created by MyGatherBK
+		clear
+echo ""
+echo -e "${RED} =============== OS-32 & 64-bit =================    "
+echo -e "${RED} #    OS  DEBIAN 8-9-10  OS  UBUNTU 14-16-18    #    "
+echo -e "${RED} #         BY : Pirakit Khawpleum               #    "
+echo -e "${RED} #    FB : https://m.me/pirakrit.khawplum       #    "
+echo -e "${RED} =============== OS-32 & 64-bit =================    "
+echo -e "${GREEN} ไอพีเซิฟ: $IP "
+echo -e "${NC} "
+echo ""
+echo -e "${GRAY}ปรับเปลี่ยนระบบของเซิฟเวอร์ ${NC} "
+echo ""
+echo -e "|${GRAY}1${NC}| 1 ไฟล์เชื่อมต่อได้ 1 เครื่องเท่านั้น สามารถสร้างไฟล์เพิ่มได้"
+echo -e "|${GRAY}2${NC}| 1 ไฟล์เชื่อมต่อได้หลายเครื่อง แต่ต้องสร้างบัญชีเพื่อใช้เชื่อมต่อ"
+echo -e "|${GRAY}3${NC}| 1 ไฟล์เชื่อมต่อได้ไม่จำกัดเครื่อง"
+echo ""
+read -p "เลือกหัวข้อที่ต้องการใช้งาน : " CHANGESYSTEMSERVER
 
-			# This option could be documented a bit better and maybe even be simplified
-			# ...but what can I say, I want some sleep too
-			number_of_clients=$(tail -n +2 /etc/openvpn/server/easy-rsa/pki/index.txt | grep -c "^V")
-			if [[ "$number_of_clients" = 0 ]]; then
-				echo
-				echo "There are no existing clients!"
-				exit
-			fi
-			echo
-			echo "Select the client to revoke:"
-			tail -n +2 /etc/openvpn/server/easy-rsa/pki/index.txt | grep "^V" | cut -d '=' -f 2 | nl -s ') '
-			read -p "Client: " client_number
-			until [[ "$client_number" =~ ^[0-9]+$ && "$client_number" -le "$number_of_clients" ]]; do
-				echo "$client_number: invalid selection."
-				read -p "Client: " client_number
-			done
-			client=$(tail -n +2 /etc/openvpn/server/easy-rsa/pki/index.txt | grep "^V" | cut -d '=' -f 2 | sed -n "$client_number"p)
-			echo
-			read -p "Confirm $client revocation? [y/N]: " revoke
-			until [[ "$revoke" =~ ^[yYnN]*$ ]]; do
-				echo "$revoke: invalid selection."
-				read -p "Confirm $client revocation? [y/N]: " revoke
-			done
-			if [[ "$revoke" =~ ^[yY]$ ]]; then
-				cd /etc/openvpn/server/easy-rsa/
-				./easyrsa --batch revoke "$client"
-				EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl
-				rm -f /etc/openvpn/server/crl.pem
-				cp /etc/openvpn/server/easy-rsa/pki/crl.pem /etc/openvpn/server/crl.pem
-				# CRL is read with each client connection, when OpenVPN is dropped to nobody
-				chown nobody:"$group_name" /etc/openvpn/server/crl.pem
-				echo
-				echo "$client revoked!"
-			else
-				echo
-				echo "$client revocation aborted!"
-			fi
-			exit
+case $CHANGESYSTEMSERVER in
+
+	1)
+
+sed -i '28d' /etc/openvpn/server/server.conf
+sed -i '28d' /etc/openvpn/server/server.conf
+sed -i '28d' /etc/openvpn/server/server.conf
+sed -i '20d' /etc/openvpn/server/client-common.txt
+echo "client-to-client" >> /etc/openvpn/server/server.conf
+echo ""
+echo "ปรับเปลี่ยนระบบของเซิฟเวอร์เป็นรูปแบบที่ 1 เรียบร้อย"
+echo ""
+service openvpn restart
+
+	;;
+
+	2)
+
+sed -i '28d' /etc/openvpn/server/server.conf
+sed -i '28d' /etc/openvpn/server/server.conf
+sed -i '28d' /etc/openvpn/server/server.conf
+sed -i '20d' /etc/openvpn/server/client-common.txt
+if [[ "$VERSION_ID" = 'VERSION_ID="7"' ]]; then
+	echo "plugin /usr/lib/openvpn/openvpn-auth-pam.so /etc/pam.d/login" >> /etc/openvpn/server/server.conf
+	echo "client-cert-not-required" >>  /etc/openvpn/server/server.conf
+	echo "username-as-common-name" >>  /etc/openvpn/server/server.conf
+else
+	echo "plugin /usr/lib/openvpn/openvpn-plugin-auth-pam.so /etc/pam.d/login" >> /etc/openvpn/server/server.conf
+	echo "client-cert-not-required" >> /etc/openvpn/server/server.conf
+	echo "username-as-common-name" >>  /etc/openvpn/server/server.conf
+fi
+echo "auth-user-pass" >> /etc/openvpn/server/client-common.txt
+echo ""
+echo "ปรับเปลี่ยนระบบของเซิฟเวอร์เป็นรูปแบบที่ 2 เรียบร้อย"
+echo ""
+service openvpn restart
+
+	;;
+
+	3)
+
+sed -i '28d' /etc/openvpn/server/server.conf
+sed -i '28d' /etc/openvpn/server/server.conf
+sed -i '28d' /etc/openvpn/server/server.conf
+sed -i '20d' /etc/openvpn/server/client-common.txt
+echo "duplicate-cn" >> /etc/openvpn/server.conf
+echo ""
+echo "ปรับเปลี่ยนระบบของเซิฟเวอร์เป็นรูปแบบที่ 3 เรียบร้อย"
+echo ""
+service openvpn restart
+
+	;;
+
+esac
+
+	;;
